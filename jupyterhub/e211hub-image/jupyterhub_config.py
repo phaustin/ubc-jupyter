04a120b4bff8ada209f541aeae22657a61383270
Phil Austin <paustin@eoas.ubc.ca>
2022-07-14 16:31:04 -0700

    reify

--- jupyterhub/e211hub-image/jupyterhub_config.py
+++ jupyterhub/e211hub-image/jupyterhub_config.py
@@ -13,20 +13,18 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.hub_ip = '0.0.0.0'
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
-c.JupyterHub.hub_connect_ip = 'e211hub'
+c.JupyterHub.hub_connect_ip = 'oauthe211hub'
 
 # pick a docker image. This should have the same version of jupyterhub
 # two identical entries here to show image choice pulldown menue
-c.DockerSpawner.allowed_images = {'e211' : 'phaustin/e211book:may27',
-                                  'a405' : 'phaustin/e211book:may27'}
+c.DockerSpawner.allowed_images = {'e211' : 'phaustin/e211-notebook:latest',
+                                  'e350' : 'phaustin/e211-notebook:latest'}
 notebook_dir = "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # tell the user containers to connect to our docker network
-c.DockerSpawner.network_name = 'traefik_net'
-c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir,
-                          "/home/jovyan/repos/ansible_intro/jupyterhub/data_share": 
-                           {"bind": "/home/jovyan/work/data_share", "mode": "rw"}}
+c.DockerSpawner.network_name = 'proxy_aug07'
+c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
 
 # delete containers when the stop
 c.DockerSpawner.remove = True
