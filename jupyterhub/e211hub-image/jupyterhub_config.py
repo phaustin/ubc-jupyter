3fd5ea7651e51d790fbf5f8c5e09e23353bac760
Phil Austin <paustin@eos.ubc.ca>
2022-07-19 13:43:22 -0700

    add e350 container

--- jupyterhub/e211hub-image/jupyterhub_config.py
+++ jupyterhub/e211hub-image/jupyterhub_config.py
@@ -18,7 +18,7 @@ c.JupyterHub.hub_connect_ip = 'oauthe211hub'
 # pick a docker image. This should have the same version of jupyterhub
 # two identical entries here to show image choice pulldown menue
 c.DockerSpawner.allowed_images = {'e211' : 'jovyan/e211-notebook:latest',
-                                  'e350' : 'jovyan/e211-notebook:latest'}
+                                  'e350' : 'jovyan/e350-notebook:latest'}
 notebook_dir = "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
